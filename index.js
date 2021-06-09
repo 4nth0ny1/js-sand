86ad333729e11e0c5b9b466a9bc6e9bb702c8482
Anthony Catullo <acatullo4@gmail.com>
2021-06-08 19:42:16 -0500

    updated html

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("test");
+
+
+const node = document.createElement("h1");
+
+const textNode = document.createTextNode(":-)");
+
+node.appendChild(textNode);
+
+document.querySelector('.myList').appendChild(node);
